fc0c3e8b5649e57fb613cdc6630e45f0a0d298cc
TMWtechLAB <techlab@tmw.at>
2022-12-21 14:32:36 +0100

    Added techLAB branding

--- KeyNStroke/Properties/AssemblyInfo.cs
+++ KeyNStroke/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Key'n'Stroke")]
+[assembly: AssemblyTitle("Key'n'Stroke techLAB Edition")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Key'n'Stroke")]
+[assembly: AssemblyProduct("Key'n'Stroke techLAB Edition")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.1")]
 //[assembly: AssemblyVersion("1.1.0.0")]
 //[assembly: AssemblyFileVersion("1.1.0.0")]
